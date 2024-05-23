3cf6128b9c335598d6a00c003dd4f47ae13f1a60
jadavPintu28 <jadavpintu43@gmail.com>
2024-05-23 11:31:38 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+setInterval(() =>{
+    let currentTime = new Date()
+
+    hours.innerHTML = (currentTime.getHours()<10?"0":"")+currentTime.getHours();
+    minutes.innerHTML = (currentTime.getMinutes()<10?"0":"")+currentTime.getMinutes();
+    seconds.innerHTML = (currentTime.getSeconds()<10?"0":"")+currentTime.getSeconds();
+},1000)
